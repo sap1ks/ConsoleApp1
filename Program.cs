a9805079792bb60c874eab9443b6fb526dfe493c
sap1ks <sap1kcsgo@gmail.com>
2024-10-22 09:52:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("number1");
+            int number1=int.Parse(Console.ReadLine());
+            Console.Write("number2=");
+            int number2 = int.Parse(Console.ReadLine());
+            int sum = number1 + number2;
+            Console.WriteLine($"sum= {sum}";
+            Console.Read();
+        }
+    }
+}
